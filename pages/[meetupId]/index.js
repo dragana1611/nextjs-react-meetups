3221369647658faf1f491a40d9e47cfd3ee8425f
Dragana Petrovic <dragana.popov-petrovic@badin.rs>
2023-05-18 01:32:25 +0200

    SSG, Server-side Rendering, params for SSG, getStaticPaths

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,6 +1,6 @@
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-const MeetupDetails = () => {
+function MeetupDetails() {
   return (
     <MeetupDetail
       image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
@@ -9,6 +9,47 @@ const MeetupDetails = () => {
       description="This is a first meetup"
     />
   );
-};
+}
+
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  // fetch data for a single meetup
+
+  const meetupId = context.params.meetupId;
+
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+        id: meetupId,
+        title: "First Meetup",
+        address: "Some Street 5, Some City",
+        description: "This is a first meetup",
+      },
+    },
+  };
+}
 
 export default MeetupDetails;
+
+//getStaticPaths  is another important function, which we need in dynamic pages to tell NextJS for which dynamic parameter values this page should be pre-generated. And then again, getStaticProps executes for every page.
--- pages/index.js
+++ pages/index.js
@@ -21,8 +21,36 @@ const DUMMY_MEETUPS = [
   },
 ];
 
-function HomePage() {
-  return <MeetupList meetups={DUMMY_MEETUPS} />;
+function HomePage(props) {
+  // return <MeetupList meetups={DUMMY_MEETUPS} />;
+  return <MeetupList meetups={props.meetups} />;
 }
 
+export const getStaticProps = async () => {
+  //fetch data from an API/DB
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
+    revalidate: 10, // if data change more frequently, there is an extra property. When we add this property to the object returned by getStaticProps, we unlock a feature called incremental Static Generation. This number is the number of seconds NextJS will wait until it regenerates this page for an incoming request.
+  };
+};
+
+// //This now only works in our page component files, not in other component files. Only in component files inside of the pages folder.  NextJS will look for a function with that name and if it finds it, it executes this function during this pre-rendering process. The code in this f-on will never be executed on the client side. Returns an object with static props
+// //One of the main features of NextJS, this data fetching for pre-rendering.
+
+// export const getServerSideProps = (context) => {
+//   const req = context.req;
+//   const res = context.res;
+
+//   //fetch data from an API/DB
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS,
+//     },
+//   };
+// };
+
 export default HomePage;
+
+//We should only use getServerSideProps if we need access to that concrete request object.  Or if we have data that changes multiple times every second, then therefore even revalidate won't help us, then getServerSideProps is a great choice.
